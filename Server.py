2eb4c7fa22461c2b9a0d1b1c13610b8811124d93
Carlos-Bernalte <carlos.bernalte@outlook.com>
2020-11-26 16:33:18 +0100

    Validación del token para publicar y eliminar mapa

--- Server.py
+++ Server.py
@@ -9,25 +9,39 @@ import IceGauntlet
 
 class RoomI(IceGauntlet.Room):
     n = 0
+    def __init__(self, proxy_auth_server):
+        
+        self.auth_server = IceGauntlet.AuthenticationPrx.checkedCast(proxy_auth_server)
+
+        if not self.auth_server:
+            raise RuntimeError('Invalid proxy for authentification server')
 
     def getRoom(self, current=None):
         level= open("server_maps/my_map.json", mode='r', encoding='utf-8')
         return level.read()
 
     def publish(self,token, new_room, current=None):
-        archivo = open("server_maps/mapa_nuevo.json","w")
-        archivo.write(new_room)
-        print("Mapa publicado")
-        archivo.close()
+        
+        if self.auth_server.isValid(token):
+            archivo = open("server_maps/mapa_nuevo.json","w")
+            archivo.write(new_room)
+            print("Mapa publicado")
+            archivo.close()
+        else:
+            print("RoomAlreadyExists")
 
     def remove(self,token, room_name, current=None):
-        remove(os.path.join("{0}/server_maps/{1}".format(os.getcwd(), room_name)))
+        if self.auth_server.isValid(token):
+            remove(os.path.join("{0}/server_maps/{1}".format(os.getcwd(), room_name)))
+        else:
+            print("No puedes")
 
 
 class Server(Ice.Application):
     def run(self, argv):
         broker = self.communicator()
-        servant = RoomI()
+
+        servant = RoomI(self.communicator().stringToProxy(argv[1]))
 
         adapter = broker.createObjectAdapter("ServerAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("room1"))
