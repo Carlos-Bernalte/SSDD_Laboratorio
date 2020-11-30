6b3ace14f745e8cca28696eccc4cfd4dc512dfc5
Carlos-Bernalte <carlos.bernalte@outlook.com>
2020-11-30 11:18:23 +0100

    Cambios en la organización de los archivps

---
+++ Clients/ClientGameService.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import Ice
+import json
+Ice.loadSlice('icegauntlet.ice')
+import IceGauntlet
+
+class Client(Ice.Application):
+
+    def run(self, argv):
+        print(argv[1])
+        proxy = self.communicator().stringToProxy(argv[1])
+        self.room = IceGauntlet.RoomPrx.checkedCast(proxy)
+        if not self.room:
+            raise RuntimeError('Invalid proxy')
+        
+        roomData=self.room.getRoom()
+        roomDataJson = json.load(roomData)
+        archivo = open("client_maps/"+str(roomDataJson['room']),"w")
+        archivo.write(roomData)
+        archivo.close()
+        
+        return 0
+        
+sys.exit(Client().main(sys.argv))
