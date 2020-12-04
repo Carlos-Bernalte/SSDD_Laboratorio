27a4d3bcb0b02704f0275b2458ab383227572e03
JuanMC00 <Juan.Munoz7@alu.uclm.es>
2020-12-04 10:54:20 +0100

    Introducida carpeta del juego

---
+++ icegauntlet-master/editor/map_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="map_tiles" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
+ <image source="map_tiles.png" width="256" height="256"/>
+</tileset>
